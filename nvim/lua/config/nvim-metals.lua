4d2aff9f85b1ec8fd56df620259781b942811395
killbirds <killbirds@gmail.com>
2023-03-11 17:16:48 +0900

    scala metals: 0.11.11

--- nvim/lua/config/nvim-metals.lua
+++ nvim/lua/config/nvim-metals.lua
@@ -6,7 +6,7 @@ local metals_config = require('metals').bare_config()
 
 -- Example of settings
 metals_config.settings = {
-  serverVersion = "0.11.10",
+  serverVersion = "0.11.11",
   showImplicitArguments = true,
   ammoniteJvmProperties = {"-Xmx2G"},
   bloopSbtAlreadyInstalled = true,
